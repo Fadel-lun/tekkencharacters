95e752949400753f88ffc6b57cd6b2589073e6bf
MuhammadFadll <fadeljourney@gmail.com>
2021-03-16 11:00:16 +0700

    mencoba menggunaka .gitignore

--- js/script.js
+++ js/script.js
@@ -1,94 +1,85 @@
-const male = document.querySelector('.male')
-const female = document.querySelector('.female')
-const input = document.querySelector('input')
-const submit = document.querySelector('.btn-outline-success')
-const row = document.querySelector('.row'); 
-const body = document.querySelector('body')
+const male = document.querySelector(".male");
+const female = document.querySelector(".female");
+const input = document.querySelector("input");
+const submit = document.querySelector(".btn-outline-success");
+const row = document.querySelector(".row");
+const body = document.querySelector("body");
 
-    // mengambil input untuk melakukan pencarian
-    const searching = () => {
+// mengambil input untuk melakukan pencarian
+const searching = () => {
+  // "new DataSource" ada di file dataSource.js
+  const data = new DataSource(succes, failed);
+  data.search(input.value);
+  body.classList.remove("pink");
+};
 
-        // "new DataSource" ada di file dataSource.js
-        const data = new DataSource(succes, failed);
-        data.search(input.value)
-        body.classList.remove('pink')
+const succes = (result) => {
+  row.innerHTML = "";
+  let names = [];
+  result.forEach((org) => {
+    if (org.name) {
+      names.push(org.name);
+      names.sort();
     }
+  });
 
-    const succes = result => {
-        row.innerHTML = '';
-        let names = [];
-            result.forEach(org => {
-                if(org.name){
-                    names.push(org.name)
-                    names.sort()
-                }
-            })
-
-
-        names.forEach(n => {
-        row.innerHTML += `<div class="col-6 col-sm-3">
+  names.forEach((n) => {
+    row.innerHTML += `<div class="col-6 col-sm-3">
                             <div class="card ${n}" >
-                                <img class='card-img-top' src="../img/${n}.jpg"  data-toggle="modal" data-target="#exampleModal">
+                                <img class='card-img-top' src="img/${n}.jpg"  data-toggle="modal" data-target="#exampleModal">
                                 <div class="card-body" style="padding: 12px;">
                                 <p class="card-text">${n}</p>
                                 </div>
                             </div>
-                        </div>`
-                    })
-        showModal(row.innerHTML)
-                
-    }
+                        </div>`;
+  });
+  showModal(row.innerHTML);
+};
 
-    const failed = result => {
-        row.innerHTML = `<h3 style="margin : 0 auto;">${result}</h3>`;
+const failed = (result) => {
+  row.innerHTML = `<h3 style="margin : 0 auto;">${result}</h3>`;
+};
 
-    }
-
-    submit.addEventListener('click', searching)
+submit.addEventListener("click", searching);
 
 // klik female
-female.addEventListener('click',function(){
-    const filterGenders = org.filter(o => {
-        return o.gender
-    })
-    body.classList.add('pink')
-    succes(filterGenders)
-})
+female.addEventListener("click", function () {
+  const filterGenders = org.filter((o) => {
+    return o.gender;
+  });
+  body.classList.add("pink");
+  succes(filterGenders);
+});
 
 // klik male
-male.addEventListener('click', function(){
-    const filterGenders = org.filter(o => {
-        return 'female' != o.gender
-    })
-    body.classList.remove('pink')
-    succes(filterGenders)
-})
-
-
-
+male.addEventListener("click", function () {
+  const filterGenders = org.filter((o) => {
+    return "female" != o.gender;
+  });
+  body.classList.remove("pink");
+  succes(filterGenders);
+});
 
 // modal box
 const showModal = () => {
-    const modalBox = document.querySelector('.modal')
-    const img = document.querySelectorAll('img');
+  const modalBox = document.querySelector(".modal");
+  const img = document.querySelectorAll("img");
 
-    let infoName = ''; 
-    img.forEach(i => {
-        org.forEach(o => {
-            i.addEventListener('click', function(){
-               if(i.getAttribute('src') == `../img/${o.name}.jpg`){
-                   console.log(i)
-                   infoName = showInfo(o.name, o.info);
-                   modalBox.innerHTML = infoName;
-               }
-            })
-    
-        })
-    })
+  let infoName = "";
+  img.forEach((i) => {
+    org.forEach((o) => {
+      i.addEventListener("click", function () {
+        if (i.getAttribute("src") == `img/${o.name}.jpg`) {
+          console.log(i);
+          infoName = showInfo(o.name, o.info);
+          modalBox.innerHTML = infoName;
+        }
+      });
+    });
+  });
 
-
-    function showInfo(n, i){
-        return ` <div class="modal-dialog modal-lg " role="document">
+  function showInfo(n, i) {
+    return ` <div class="modal-dialog modal-lg " role="document">
         <div class="modal-content">
                         <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">Character Details : ${n} </h5>
@@ -98,7 +89,7 @@ const showModal = () => {
                         </div>
                         <div class="modal-body">
                             <nav>
-                                <img src="../img/${n}.jpg">
+                                <img src="img/${n}.jpg">
                             </nav>
                             <main>
                                <h5>Description :</h5>
@@ -109,11 +100,9 @@ const showModal = () => {
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                         </div>
                         </div>
-                    </div>`
-    }
+                    </div>`;
+  }
+};
 
-}    
-                
 const a = 2;
-b = a < 5 ? 'anda benar' : 'anda salah';
-
+b = a < 5 ? "anda benar" : "anda salah";
